90af2b2191e057a4cf50ab27b477464b76ab551d
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-06-30 23:49:09 +0200

    a simple decode using Schema

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-import { Console, Effect } from "effect";
+import { Console, Effect, pipe, Schema } from "effect";
 
-const program = Console.log("ghlog-ts-effect");
+const Repo = Schema.Struct({
+    name: Schema.String,
+    url: Schema.URL,
+});
+
+type Repo = Schema.Schema.Type<typeof Repo>;
+
+function formatRepo(repo: Repo): string {
+    return `repo=${repo.name}, url=${repo.url.pathname}`;
+}
+
+function demo(input: unknown): Effect.Effect<void, never, never> {
+    return pipe(
+        Schema.decodeUnknown(Repo)(input),
+        Effect.map(formatRepo),
+        Effect.matchEffect({
+            onSuccess: (repo) => Console.log(`success: ${repo}`),
+            onFailure: (error) => Console.log(`error: ${error}`),
+        }),
+    );
+}
+
+const input: unknown = {
+    name: "ghlog-ts-effect",
+    url: "https://github.com/dhth/ghlog-ts-effect",
+};
+const program = demo(input);
 
 Effect.runSync(program);
